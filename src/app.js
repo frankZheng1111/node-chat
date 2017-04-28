b8da06f3656c1a15ceab8568def78b9b07706525
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-04-28 23:46:34 +0800

    Add app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,74 @@
 'use strict';
+import express from 'express';
+// import favicon from 'serve-favicon';
+import bodyParser from 'body-parser';
+import methodOverride from 'method-override';
+import cookieParser from 'cookie-parser';
+
+import http from 'http';
+import path from 'path';
+import log4js from 'log4js';
+import socketIo from 'socket.io';
+// import routes from './routes';
+// import user from './routes/user';
 import logger from './libs/logger';
 
-logger.info('Hello socket!');
+let app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views',path.join(__dirname, '../views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, '../public')));
+// app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
+app.use(methodOverride((req) => {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    let method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
+
+app.use(log4js.connectLogger(logger, {level:'debug', format:':method :url'}));
+
+let users = {};//存储在线用户列表的对象
+
+app.get('/', function (req, res) {
+  if (req.cookies.user == null) {
+    res.redirect('/signin');
+  } else {
+    res.render('index');
+  }
+});
+
+app.get('/signin', function (req, res) {
+  res.render('signin');
+});
+
+app.post('/signin', function (req, res) {
+  if (users[req.body.name]) {
+    //存在，则不允许登陆
+    res.redirect('/signin');
+  } else {
+    //不存在，把用户名存入 cookie 并跳转到主页
+    res.cookie('user', req.body.name, {maxAge: 1000*60*60*24*30});
+    res.redirect('/');
+  }
+});
+
+let server = http.createServer(app);
+
+let io = socketIo.listen(server);
+
+io.sockets.on('connection', function (socket) {
+});
+
+server.listen(app.get('port'), function(){
+  logger.info('Express server listening on port ' + app.get('port'));
+});
