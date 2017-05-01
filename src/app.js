8b6fd37b907b43ff0163b3d96ffbf66f4b264278
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-05-01 12:37:15 +0800

    Add user list

--- src/app.js
+++ src/app.js
@@ -67,6 +67,17 @@ let server = http.createServer(app);
 let io = socketIo.listen(server);
 
 io.sockets.on('connection', function (socket) {
+  //有人上线
+  socket.on('online', function (data) {
+    //将上线的用户名存储为 socket 对象的属性，以区分每个 socket 对象，方便后面使用
+    socket.name = data.user;
+    //users 对象中不存在该用户名则插入该用户名
+    if (!users[data.user]) {
+      users[data.user] = data.user;
+    }
+    //向所有用户广播该用户上线信息
+    io.sockets.emit('online', {users: users, user: data.user});
+  });
 });
 
 server.listen(app.get('port'), function(){
