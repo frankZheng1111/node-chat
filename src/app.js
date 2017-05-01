be68ae23d77883f93fab1d9b02c61c7b8c84f854
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-05-01 19:58:24 +0800

    Add socket folder

--- src/app.js
+++ src/app.js
@@ -8,7 +8,9 @@ import http from 'http';
 import path from 'path';
 import log4js from 'log4js';
 import socketIo from 'socket.io';
+
 import logger from './libs/logger';
+import sockets from './sockets';
 
 let app = express();
 
@@ -30,9 +32,11 @@ app.use(methodOverride((req) => {
 }));
 app.use(log4js.connectLogger(logger, {level:'debug', format:':method :url'}));
 
-//存储在线用户列表的对象
+// 存储在线用户列表的对象
+// 防止相同用户名的用户登入
+// 当前简单的放在内存里，实际项目根据具体持久化方案判断
 //
-let users = {};
+global.users = {};
 
 app.get('/', (req, res) => {
   if (req.cookies.user == null) {
@@ -47,7 +51,7 @@ app.get('/signin', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-  if (users[req.body.name]) {
+  if (global.users[req.body.name]) {
     //存在，则不允许登陆
     //
     res.redirect('/signin');
@@ -64,57 +68,7 @@ let server = http.createServer(app);
 let io = socketIo.listen(server);
 
 io.sockets.on('connection', (socket) => {
-  //有人上线
-  //
-  socket.on('online', (data) => {
-    //将上线的用户名存储为 socket 对象的属性，以区分每个 socket 对象，方便后面使用
-    //
-    socket.name = data.user;
-    //users 对象中不存在该用户名则插入该用户名
-    //
-    if (!users[data.user]) { users[data.user] = data.user; }
-    //向所有用户广播该用户上线信息
-    //
-    io.sockets.emit('online', {users: users, user: data.user});
-  });
-
-  //有人发话
-  //
-  socket.on('say', (data) => {
-    if (data.to == 'all') {
-      //向其他所有用户广播该用户发话信息
-      //
-      socket.broadcast.emit('say', data);
-    } else {
-      //向特定用户发送该用户发话信息
-      //clients 为存储所有连接对象的数组
-      //
-      // let clients = io.sockets.clients();
-      let clients = io.sockets.sockets;
-      //遍历找到该用户
-      //
-      for(let key in clients ) {
-        //触发该用户客户端的 say 事件
-        //
-        if (clients[key].name == data.to) { clients[key].emit('say', data); }
-      }
-    }
-  });
-
-  //有人下线
-  //
-  socket.on('disconnect', () => {
-    //若 users 对象中保存了该用户名
-    //
-    if (users[socket.name]) {
-      //从 users 对象中删除该用户名
-      //
-      delete users[socket.name];
-      //向其他所有用户广播该用户下线信息
-      //
-      socket.broadcast.emit('offline', {users: users, user: socket.name});
-    }
-  });
+  sockets(socket, io);
 });
 
 server.listen(app.get('port'), () => {
---
+++ src/sockets/index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+  //有人上线
+export default function (socket, io) {
+  //
+  socket.on('online', (data) => {
+    //将上线的用户名存储为 socket 对象的属性，以区分每个 socket 对象，方便后面使用
+    //
+    socket.name = data.user;
+    //global.users 对象中不存在该用户名则插入该用户名
+    //
+    if (!global.users[data.user]) { global.users[data.user] = data.user; }
+    //向所有用户广播该用户上线信息
+    //
+    io.sockets.emit('online', {users: global.users, user: data.user});
+  });
+
+  //有人发话
+  //
+  socket.on('say', (data) => {
+    if (data.to == 'all') {
+      //向其他所有用户广播该用户发话信息
+      //
+      socket.broadcast.emit('say', data);
+    } else {
+      //向特定用户发送该用户发话信息
+      //clients 为存储所有连接对象的数组
+      //
+      // let clients = io.sockets.clients();
+      let clients = io.sockets.sockets;
+      //遍历找到该用户
+      //
+      for(let key in clients ) {
+        //触发该用户客户端的 say 事件
+        //
+        if (clients[key].name == data.to) { clients[key].emit('say', data); }
+      }
+    }
+  });
+
+  //有人下线
+  //
+  socket.on('disconnect', () => {
+    //若 global.users 对象中保存了该用户名
+    //
+    if (global.users[socket.name]) {
+      //从 global.users 对象中删除该用户名
+      //
+      delete global.users[socket.name];
+      //向其他所有用户广播该用户下线信息
+      //
+      socket.broadcast.emit('offline', {users: global.users, user: socket.name});
+    }
+  });
+}
